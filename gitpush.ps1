fef51dc96cadcd48eedb9341231fc394604f09fe
Kevin Wang <kevinwang09@foxmail.com>
2022-08-07 23:14:01 +0800

    rhel

--- gitpush.ps1
+++
@@ -1,4 +0,0 @@
-git add .
-[string]$commitMessage = Read-Host -Prompt "Enter the commit message"
-git commit -m $commitMessage
-git push
--- huawei/huawei_interview2/test.sh
+++
@@ -1,2 +0,0 @@
-awk 'NR==1 {print($0)}' test.py
-sed -n '1p' test.py
